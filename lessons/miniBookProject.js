fd4711895b7e1559a9021761023eb027979b9a2d
RustamovRRR <rustamrrustamov@gmail.com>
2022-01-04 21:40:07 +0400

    mini book project with nested components

---
+++ lessons/miniBookProject.js
@@ -0,0 +1,40 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// function Booklist() {
+//   return (
+//     <div>
+//       <Book />
+//       <Book />
+//       <Book />
+//       <Book />
+//       <Book />
+//     </div>
+//   );
+// }
+
+// const Book = () => {
+//   return (
+//     <div>
+//       <Image />
+//       <Title />
+//       <Author />
+//     </div>
+//   );
+// };
+
+// const Image = () => (
+//   <img
+//     src="https://images-na.ssl-images-amazon.com/images/I/51DItJYatoL._SX324_BO1,204,203,200_.jpg"
+//     alt=""
+//   />
+// );
+
+// const Title = () => <h1>The Brothers Karamazov</h1>;
+// const Author = () => <h4>Fyodor Dostoevsky</h4>;
+
+// // const Greeting = () => {
+// //   return React.createElement("h1", {}, "Hello Piter");
+// // };
+
+// ReactDOM.render(<Booklist />, document.getElementById("root"));
--- src/index.js
+++ src/index.js
@@ -1,22 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function Greeting() {
+function Booklist() {
   return (
     <div>
-      <Person />
-      <Message />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
     </div>
   );
 }
 
-const Person = () => <h1>John Doe</h1>;
-const Message = () => {
-  return <p>this is my message</p>;
+const Book = () => {
+  return (
+    <div>
+      <Image />
+      <Title />
+      <Author />
+    </div>
+  );
 };
+
+const Image = () => (
+  <img
+    src="https://images-na.ssl-images-amazon.com/images/I/51DItJYatoL._SX324_BO1,204,203,200_.jpg"
+    alt=""
+  />
+);
+
+const Title = () => <h1>The Brothers Karamazov</h1>;
+const Author = () => <h4>Fyodor Dostoevsky</h4>;
 
 // const Greeting = () => {
 //   return React.createElement("h1", {}, "Hello Piter");
 // };
 
-ReactDOM.render(<Greeting />, document.getElementById("root"));
+ReactDOM.render(<Booklist />, document.getElementById("root"));
