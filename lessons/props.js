4e1a29e9f13942460bcc4b600bfe9cf08ec0e7f8
RustamovRRR <rustamrrustamov@gmail.com>
2022-01-04 23:52:50 +0400

    working with props

---
+++ lessons/props.js
@@ -0,0 +1,53 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// // CSS
+// import "./index.css";
+
+// // setup variables
+
+// const firstBook = {
+//   title: "The Brothers Karamazov",
+//   author: "Fyodor Dostoevsky",
+//   imgURL:
+//     "https://images-na.ssl-images-amazon.com/images/I/51DItJYatoL._SX324_BO1,204,203,200_.jpg",
+// };
+// const secondBook = {
+//   title: "The Sandman: Act II",
+//   author: "Neil Gaiman",
+//   imgURL: "https://m.media-amazon.com/images/I/51UYl4VBMRL.jpg",
+// };
+
+// function Booklist() {
+//   return (
+//     <section>
+//       <Book
+//         url={firstBook.imgURL}
+//         title={firstBook.title}
+//         author={firstBook.author}
+//       />
+//       <Book
+//         url={secondBook.imgURL}
+//         title={secondBook.title}
+//         author={secondBook.author}
+//       />
+//     </section>
+//   );
+// }
+
+// const Book = (props) => {
+//   console.log(props);
+//   return (
+//     <article>
+//       <img src={props.url} alt="" />
+//       <h1>{props.title}</h1>
+//       <h4>{props.author}</h4>
+//     </article>
+//   );
+// };
+
+// // const Greeting = () => {
+// //   return React.createElement("h1", {}, "Hello Piter");
+// // };
+
+// ReactDOM.render(<Booklist />, document.getElementById("root"));
--- src/index.js
+++ src/index.js
@@ -4,25 +4,44 @@ import ReactDOM from "react-dom";
 // CSS
 import "./index.css";
 
+// setup variables
+
+const firstBook = {
+  title: "The Brothers Karamazov",
+  author: "Fyodor Dostoevsky",
+  imgURL:
+    "https://images-na.ssl-images-amazon.com/images/I/51DItJYatoL._SX324_BO1,204,203,200_.jpg",
+};
+const secondBook = {
+  title: "The Sandman: Act II",
+  author: "Neil Gaiman",
+  imgURL: "https://m.media-amazon.com/images/I/51UYl4VBMRL.jpg",
+};
+
 function Booklist() {
   return (
     <section>
-      <Book />
+      <Book
+        url={firstBook.imgURL}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        url={secondBook.imgURL}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 }
 
-const Book = () => {
-  const title = "The Brothers Karamazov";
-  const author = "Fyodor Dostoevsky";
+const Book = (props) => {
+  console.log(props);
   return (
     <article>
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/51DItJYatoL._SX324_BO1,204,203,200_.jpg"
-        alt=""
-      />
-      <h1>{title}</h1>
-      <h4>{author.toUpperCase()}</h4>
+      <img src={props.url} alt="" />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
     </article>
   );
 };
