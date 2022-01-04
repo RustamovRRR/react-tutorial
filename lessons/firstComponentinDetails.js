1f1eaaf7497a412598ee3a6b873ae0608ba9b416
RustamovRRR <rustamrrustamov@gmail.com>
2022-01-04 18:46:59 +0400

    first component in details. using react.createelement

---
+++ lessons/firstComponentinDetails.js
@@ -0,0 +1,19 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// // stateless functional component
+// // always return JSX
+
+// // function Greeting() {
+// //   return (
+// //     <div>
+// //       <h1>Hello World</h1>
+// //     </div>
+// //   );
+// // }
+
+// const Greeting = () => {
+//   return React.createElement("h1", {}, "Hello Piter");
+// };
+
+// ReactDOM.render(<Greeting />, document.getElementById("root"));
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// stateless functional component
+// always return JSX
+
 function Greeting() {
-  return <h4>My name is Rustam and this is my first component</h4>;
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
 }
+
+// const Greeting = () => {
+//   return React.createElement("h1", {}, "Hello Piter");
+// };
 
 ReactDOM.render(<Greeting />, document.getElementById("root"));
