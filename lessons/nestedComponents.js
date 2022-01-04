f224ac615761148d2ed84d16532ba786eaf8224d
RustamovRRR <rustamrrustamov@gmail.com>
2022-01-04 21:15:41 +0400

    nested components

---
+++ lessons/nestedComponents.js
@@ -0,0 +1,30 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// // JSX rules
+// // return single element
+// // div / section / article or Fragment
+// // use camelCase for html attribute
+// // className instead of class
+// // close every element
+// // formatting
+
+// function Greeting() {
+//   return (
+//     <div>
+//       <Person />
+//       <Message />
+//     </div>
+//   );
+// }
+
+// const Person = () => <h1>John Doe</h1>;
+// const Message = () => {
+//   return <p>this is my message</p>;
+// };
+
+// // const Greeting = () => {
+// //   return React.createElement("h1", {}, "Hello Piter");
+// // };
+
+// ReactDOM.render(<Greeting />, document.getElementById("root"));
--- src/index.js
+++ src/index.js
@@ -1,31 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// JSX rules
-// return single element
-// div / section / article or Fragment
-// use camelCase for html attribute
-// className instead of class
-// close every element
-// formatting
-
 function Greeting() {
   return (
-    <>
-      <section>
-        <h1 className="heading">Hello World</h1>
-        <ul>
-          <li>
-            <a href="#">link</a>
-          </li>
-        </ul>
-      </section>
-      <section>
-        <h2>heading2</h2>
-      </section>
-    </>
+    <div>
+      <Person />
+      <Message />
+    </div>
   );
 }
+
+const Person = () => <h1>John Doe</h1>;
+const Message = () => {
+  return <p>this is my message</p>;
+};
 
 // const Greeting = () => {
 //   return React.createElement("h1", {}, "Hello Piter");
