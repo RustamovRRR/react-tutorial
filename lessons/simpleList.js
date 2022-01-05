de75b2beea628d295446a0df99ec473fe626240c
RustamovRRR <rustamrrustamov@gmail.com>
2022-01-05 22:01:49 +0400

    using simple list with map with react

---
+++ lessons/simpleList.js
@@ -0,0 +1,39 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// // CSS
+// import "./index.css";
+
+// // setup variables
+// const books = [
+//   {
+//     title: "The Brothers Karamazov",
+//     author: "Fyodor Dostoevsky",
+//     imgURL:
+//       "https://images-na.ssl-images-amazon.com/images/I/51DItJYatoL._SX324_BO1,204,203,200_.jpg",
+//   },
+//   {
+//     title: "The Sandman: Act II",
+//     author: "Neil Gaiman",
+//     imgURL: "https://m.media-amazon.com/images/I/51UYl4VBMRL.jpg",
+//   },
+// ];
+
+// const names = ["olivia", "peter", "walter"];
+// const newNames = names.map((name) => <h1>{name}</h1>);
+
+// function Booklist() {
+//   return <section>{newNames}</section>;
+// }
+
+// const Book = ({ url, title, author }) => {
+//   return (
+//     <article>
+//       <img src={url} alt="" />
+//       <h1>{title}</h1>
+//       <h4>{author}</h4>
+//     </article>
+//   );
+// };
+
+// ReactDOM.render(<Booklist />, document.getElementById("root"));
--- src/index.js
+++ src/index.js
@@ -5,60 +5,35 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 // setup variables
+const books = [
+  {
+    title: "The Brothers Karamazov",
+    author: "Fyodor Dostoevsky",
+    imgURL:
+      "https://images-na.ssl-images-amazon.com/images/I/51DItJYatoL._SX324_BO1,204,203,200_.jpg",
+  },
+  {
+    title: "The Sandman: Act II",
+    author: "Neil Gaiman",
+    imgURL: "https://m.media-amazon.com/images/I/51UYl4VBMRL.jpg",
+  },
+];
 
-const firstBook = {
-  title: "The Brothers Karamazov",
-  author: "Fyodor Dostoevsky",
-  imgURL:
-    "https://images-na.ssl-images-amazon.com/images/I/51DItJYatoL._SX324_BO1,204,203,200_.jpg",
-};
-const secondBook = {
-  title: "The Sandman: Act II",
-  author: "Neil Gaiman",
-  imgURL: "https://m.media-amazon.com/images/I/51UYl4VBMRL.jpg",
-};
+const names = ["olivia", "peter", "walter"];
+const newNames = names.map((name) => <h1>{name}</h1>);
 
 function Booklist() {
-  return (
-    <section>
-      <Book
-        url={firstBook.imgURL}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
-          perspiciatis ratione veniam beatae quibusdam optio quisquam eligendi
-          consectetur a adipisci.
-        </p>
-      </Book>
-      <Book
-        url={secondBook.imgURL}
-        title={secondBook.title}
-        author={secondBook.author}
-      >
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-          Reprehenderit similique sit tenetur!
-        </p>
-      </Book>
-    </section>
-  );
+  return <section>{newNames}</section>;
 }
 
-const Book = ({ url, title, author, children }) => {
+const Book = ({ url, title, author }) => {
   return (
     <article>
       <img src={url} alt="" />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {children}
     </article>
   );
 };
-
-// const Greeting = () => {
-//   return React.createElement("h1", {}, "Hello Piter");
-// };
 
 ReactDOM.render(<Booklist />, document.getElementById("root"));
