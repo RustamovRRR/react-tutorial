7f516f5bf230ccc2ccabd994e58f60f0a97a0f4c
RustamovRRR <rustamrrustamov@gmail.com>
2022-01-06 20:27:44 +0400

    export import statements

---
+++ src/data.js
@@ -0,0 +1,14 @@
+export const data = [
+  {
+    id: 1,
+    title: "The Brothers Karamazov",
+    author: "Fyodor Dostoevsky",
+    url: "https://images-na.ssl-images-amazon.com/images/I/51DItJYatoL._SX324_BO1,204,203,200_.jpg",
+  },
+  {
+    id: 2,
+    title: "The Sandman: Act II",
+    author: "Neil Gaiman",
+    url: "https://m.media-amazon.com/images/I/51UYl4VBMRL.jpg",
+  },
+];
--- src/index.js
+++ src/index.js
@@ -5,55 +5,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 // setup variables
-const books = [
-  {
-    id: 1,
-    title: "The Brothers Karamazov",
-    author: "Fyodor Dostoevsky",
-    url: "https://images-na.ssl-images-amazon.com/images/I/51DItJYatoL._SX324_BO1,204,203,200_.jpg",
-  },
-  {
-    id: 2,
-    title: "The Sandman: Act II",
-    author: "Neil Gaiman",
-    url: "https://m.media-amazon.com/images/I/51UYl4VBMRL.jpg",
-  },
-];
+import { data } from "./data";
+import Book from "./book";
 
 function Booklist() {
   return (
     <section>
-      {books.map((book) => {
+      {data.map((book) => {
         return <Book key={book.id} {...book}></Book>;
       })}
     </section>
   );
 }
-
-const Book = ({ url, title, author }) => {
-  const clickHandler = () => {
-    alert("hello");
-  };
-  const clickHandler2 = (e) => {
-    console.log(e.target);
-  };
-  const complexHandler = (author) => {
-    console.log(author);
-  };
-  return (
-    <article>
-      <img src={url} alt="" />
-      <h1 onClick={clickHandler2}>{title}</h1>
-      <h4
-        onClick={() => {
-          complexHandler(author);
-        }}
-      >
-        {author}
-      </h4>
-      <button onClick={clickHandler}>alert hello</button>
-    </article>
-  );
-};
 
 ReactDOM.render(<Booklist />, document.getElementById("root"));
